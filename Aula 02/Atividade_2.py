92547733f2c2a7263b55c7f81b761ac81dc77093
Pablo-Moura <81646921+Pablo-Moura@users.noreply.github.com>
2021-03-30 20:32:09 -0300

    Atividade_2.py

---
+++ Aula 02/Atividade_2.py
@@ -0,0 +1,9 @@
+# Dada uma lista, utilize a característica de slicing para obter 3 listas de mesmo tamanho a partir da lista original. (sampleList = [11, 45, 8, 23, 14, 12, 78, 45, 89])
+
+sampleList = [11, 45, 8, 23, 14, 12, 78, 45, 89]
+
+listaUm = sampleList[0:3]
+listaDois = sampleList[3:6]
+listaTres = sampleList[6:9]
+
+print(f'{listaUm}\n{listaDois}\n{listaTres}\n')
